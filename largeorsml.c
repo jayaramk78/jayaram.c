cd59678a9b74c7ecd016bff54936e7d011216578
jayaramk78 <35652257+jayaramk78@users.noreply.github.com>
2018-01-26 09:01:08 +0000

    Create largeorsml.c

---
+++ largeorsml.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+           int a ,b,c;
+   printf("enter the value");
+   scanf("%d %d %d",&a,&b,&c);
+   if(a>b && a>c)
+   {
+       printf("the larger value is %d",a);
+       
+   }
+    if(b>a && b>c)
+   {
+       printf("the larger value is %d,b");
+       
+   }
+  else
+  {
+      printf("the larger value is %d,c");
+      
+  }
+  else
+  {
+      printf("all are equal")
+  }
+return(0);
+}
