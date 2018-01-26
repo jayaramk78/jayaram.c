8a39e6db228ba8d7ece1448b5b592e7a44bfd61b
jayaramk78 <35652257+jayaramk78@users.noreply.github.com>
2018-01-26 05:12:57 +0000

    Create alphabet or not.c

---
+++ alphabet or not.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    char c;
+    printf("enter the character");
+    scanf("%c",&c);
+    if(c>='a' && c<='z')
+    {
+    printf("it is a alphabet");
+    }
+    else if(c>='A' && c<='Z')
+    {
+    printf("it is a alphabet");
+    }
+    else
+    {
+        printf("it is not a alphabet");
+            }
+}
+        
+    
