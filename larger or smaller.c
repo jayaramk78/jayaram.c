88aa13f4290beac9087ecaf6ece274cdfd14e14e
jayaramk78 <35652257+jayaramk78@users.noreply.github.com>
2018-01-26 06:56:21 +0000

    Create larger or smaller.c

---
+++ larger or smaller.c
@@ -0,0 +1,23 @@
+include<stdio.h>
+
+int main() 
+    
+{
+    int a,b,c;
+    printf("\n Enter the values");
+    scanf("%d""%d""%d",&a,&b,&c);
+    if (a>b&c)
+    { 
+        printf("\n the larger number is %d",a);
+    }
+    else if(b>c)
+    {
+    printf("\n the larger number is %d",b );
+}
+else
+{
+    printf("\n the larger no is %d",c);
+    
+}
+
+}
