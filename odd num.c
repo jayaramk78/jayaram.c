0adece57ca31e41bb5162a4397e009b8a2efda3c
jayaramk78 <35652257+jayaramk78@users.noreply.github.com>
2018-02-06 16:23:59 +0530

    Create odd num.c

---
+++ odd num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    printf("enter the limits");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+    if(i%2!=0);
+      //continue;
+    else
+      printf("%d\n",i);
+    }
+}
