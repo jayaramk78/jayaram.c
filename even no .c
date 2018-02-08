26253891a54493b81e30a1aae7f6f0d75b017b16
jayaramk78 <35652257+jayaramk78@users.noreply.github.com>
2018-02-08 16:03:13 +0530

    Create even no .c

---
+++ even no .c
@@ -0,0 +1,13 @@
+int main()
+{
+    int a,i;
+    printf("enter the number");
+    scanf("%d",&a);
+    for(i=0;i<=a;i++)
+    {
+        if(i%2==0)
+        printf("\n even numbers %d",i);
+    }
+
+    return 0;
+}
