6a5dbc7ae5a14ada14a72d9037c3a01189fab898
jayaramk78 <35652257+jayaramk78@users.noreply.github.com>
2018-03-02 14:23:10 +0530

    Create spc.c

---
+++ spc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int count=0 ;
+    char c[500] ,i;
+    printf("\n enter the character");
+    scanf("%c",&c[i]);
+    for(i=0;i<c[i];i++)
+    if(c[i]>='!' &&c[i]<='*')
+    {
+  count=count+1;
+    }
+   printf("\n the no of charactrs is %d",count);
+    return 0;
+}
+
+
