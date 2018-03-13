28420f50a9f5f4a5f17068fda643e687b36219be
jayaramk78 <35652257+jayaramk78@users.noreply.github.com>
2018-03-13 16:26:26 +0530

    Create inrange.c

---
+++ inrange.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    printf("\n Enter the no:");
+    scanf("%d%d%d",&i,&j,&k);
+  if(i>=j && i<=k)  
+{
+    printf("\n Yes it is in range");
+    }
+    else
+    printf("\n It is not in range");
+}
